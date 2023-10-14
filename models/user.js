874224b3dc2b8775ff5ca6b5ad6f036a622f9cbe
MariaKonstantinov <konstantinovamariia1@gmail.com>
2023-10-14 20:13:06 +0300

    stage-1: user and article schema

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      // the requirements for every user name field:
+      type: String,
+      required: [true, "Enter your name please"],
+      minlength: [2, "Please lengthen this text to 2 characters or more"],
+      maxlength: [30, "Please lengthen this text to 30 characters or less"],
+    },
+    email: {
+      // the requirements for every user email field:
+      type: String,
+      required: [true, "Sorry,Email is required"],
+      unique: true,
+      validate: {
+        validator: (email) => validator.isEmail(email),
+        message: "Sorry, it is not a valid email",
+      },
+    },
+    password: {
+      // the requirements for every user password field:
+      type: String,
+      required: [true, "Password is required"],
+      select: false,
+    },
+  },
+  { versionKey: false }
+);
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password
+) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect email or password"));
+        }
+        return user;
+      });
+    });
+};
+
+module.exports = mongoose.model("user", userSchema);
